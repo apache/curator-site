5cad700b2befaf02e415845a1737b9f434b8e768
tisonkun <tisonkun@users.noreply.github.com>
2024-01-08 08:55:05 +0000

    deploy: ffbb46d13b4bd916dec778987f94c3a279170136

--- apidocs/javadoc.sh
+++ apidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/17.0.8-1/x64/bin/javadoc -J-Xmx1g @options @packages
+/opt/hostedtoolcache/Java_Zulu_jdk/21.0.1-12/x64/bin/javadoc -J-Xmx1g @options @packages
---
+++ assets/js/8855.76b150f5.js
@@ -0,0 +1,1 @@
+(self.webpackChunkcurator_site=self.webpackChunkcurator_site||[]).push([[8855],{8855:()=>{}}]);
